026cda9ecfab4efbca2c868f9fb19655e8b41b01
Rafael Silva <rafaelmike33@gmail.com>
2024-07-20 18:46:26 -0300

    Get user by id

--- src/main/java/com/rafsm1ke/springmongo/resources/UserResource.java
+++ src/main/java/com/rafsm1ke/springmongo/resources/UserResource.java
@@ -7,6 +7,7 @@ import com.rafsm1ke.springmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,6 +27,13 @@ public class UserResource {
         List<User> list = service.findAll();
         List<UserDTO> listDto = list.stream().map(UserDTO::new).toList();
         return ResponseEntity.ok(listDto);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+        User user = service.findById(id);
+
+        return ResponseEntity.ok(new UserDTO(user));
     }
 
 }
--- src/main/java/com/rafsm1ke/springmongo/services/UserService.java
+++ src/main/java/com/rafsm1ke/springmongo/services/UserService.java
@@ -2,10 +2,12 @@ package com.rafsm1ke.springmongo.services;
 
 import com.rafsm1ke.springmongo.domain.User;
 import com.rafsm1ke.springmongo.repositories.UserRepository;
+import com.rafsm1ke.springmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -15,5 +17,10 @@ public class UserService {
 
     public List<User> findAll() {
         return repo.findAll();
+    }
+
+    public User findById(String id) {
+        Optional<User> user = repo.findById(id);
+        return user.orElseThrow(() -> new ObjectNotFoundException("User not found"));
     }
 }
