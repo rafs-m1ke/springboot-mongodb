bc46116567164e2913abdbd7796a816f6b1a8b8e
Rafael Silva <rafaelmike33@gmail.com>
2024-07-20 22:04:09 -0300

    Add post comments

--- src/main/java/com/rafsm1ke/springmongo/config/Instantiation.java
+++ src/main/java/com/rafsm1ke/springmongo/config/Instantiation.java
@@ -3,6 +3,7 @@ package com.rafsm1ke.springmongo.config;
 import com.rafsm1ke.springmongo.domain.Post;
 import com.rafsm1ke.springmongo.domain.User;
 import com.rafsm1ke.springmongo.dto.AuthorDTO;
+import com.rafsm1ke.springmongo.dto.CommentDTO;
 import com.rafsm1ke.springmongo.repositories.PostRepository;
 import com.rafsm1ke.springmongo.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,7 @@ import org.springframework.context.annotation.Configuration;
 
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.List;
 import java.util.TimeZone;
 
 
@@ -41,6 +43,13 @@ public class Instantiation implements CommandLineRunner {
 
         Post post1 = new Post(null, sdf.parse("21/03/2024"), "Partiu viagem!", "Indo para SP!", new AuthorDTO(maria));
         Post post2 = new Post(null, sdf.parse("24/04/2024"), "Bom dia!", "Cafezinho quente logo cedo....", new AuthorDTO(maria));
+
+        CommentDTO c1 = new CommentDTO("Boa viagem!", sdf.parse("21/06/2024"), new AuthorDTO(alex));
+        CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/06/2024"), new AuthorDTO(bob));
+        CommentDTO c3 = new CommentDTO("Tenha um bom dia!!", sdf.parse("23/06/2024"), new AuthorDTO(alex));
+
+        post1.getComments().addAll(Arrays.asList(c1, c2));
+        post2.getComments().add(c3);
 
         postRepository.saveAll(Arrays.asList(post1, post2));
 
