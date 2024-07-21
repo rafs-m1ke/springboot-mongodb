86f7c81b8dea27b4ea85701b61a3e4e588350afe
Rafael Silva <rafaelmike33@gmail.com>
2024-07-21 15:13:23 -0300

    FIX bugs

--- src/main/java/com/rafsm1ke/springmongo/SpringmongoApplication.java
+++ src/main/java/com/rafsm1ke/springmongo/SpringmongoApplication.java
@@ -2,8 +2,11 @@ package com.rafsm1ke.springmongo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+
+@ComponentScan({ "aaa.bbb.ccc.*" })
 public class SpringmongoApplication {
 
     public static void main(String[] args) {
